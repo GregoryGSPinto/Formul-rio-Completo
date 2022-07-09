28e5ec9c9e214b0bbab93a1b20f2450ed22a7cc3
gregoryguimaraes12 <gregoryguimaraes12@gmail.com>
2022-07-09 10:34:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const email=document.getElementById('email');
 const password=document.getElementById('password');
 const passwordConfirmation=document.getElementById('password-confirmation');
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();  
+
     checkInputs();
 });
 
@@ -16,39 +17,39 @@ function checkInputs(){
     const passwordValue=password.value;
     const passwordConfirmationValue=passwordConfirmation.value;
 
-    if (usernameValue == "") {
-     setErrorFor(username, "O nome de usuário é obrigatório.");
-    } else {
+    if   (usernameValue === "") {
+        setErrorFor(username, "O nome de usuário é obrigatório.");
+    }   else {
         setSuccessFor(username);
     }
-    if (emailValue == '') {
+    if   (emailValue === '') {
         setErrorFor(email, 'O email é obrigatório.');
-    } else if (!checkEmail(emailValue)) {
+    }   else if (!checkEmail(emailValue)) {
         setErrorFor(email, 'Por favor, insira um email válido.');
-    } else {
+    }   else {
         setSuccessFor(email);
     }
-    if(passwordValue == '') {
+    if   (passwordValue === '') {
         setErrorFor(password, 'A senha é obrigatória.');
-    } else if (passwordValue.length < 7) {
+    }   else if (passwordValue.length < 7) {
         setErrorFor(password, 'A senha deve ter no mínimo 7 caracteres');
-    } else {
+    }   else {
         setSuccessFor(password);
     }
-    if(passwordConfirmationValue == '') {
+    if   (passwordConfirmationValue === '') {
         setErrorFor(passwordConfirmation, 'Confirmação de senha obrigatória.');
-    } else if (passwordConfirmationValue !== passwordValue) {
-        setErrorFor(passwordConfirmation, 'As senhas não Conferem');
-    } else {
+    }   else if (passwordConfirmationValue !== passwordValue) {
+        setErrorFor(passwordConfirmation, 'As senhas não Conferem.');
+    }   else {
         setSuccessFor(passwordConfirmation);
     }
 
-    const formControls=form.querySelectorAll('.form-control')
-    const formIsValid=[... formControls].every(formControl => {
-        return formControl.className='form-control sucess';
+    const formControls=form.querySelectorAll('.form-control');
+    const formIsValid=[... formControls].every((formControl) => {
+        return formControl.className ==='form-control sucess';
     });
     if (formIsValid){
-        console.log('O formulário está 100% Válido!');
+      console.log('O formulário está 100% Válido!');
     }
 }
 function setErrorFor(input, message){
@@ -66,5 +67,10 @@ function setSuccessFor(input){
     const formControl=input.parentElement;
 
     //Adicionar a classe de sucesso
-    formControl.className='form-control sucess';
+    formControl.className='form-control success';
 }
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    .test(email
+    );
+  }
